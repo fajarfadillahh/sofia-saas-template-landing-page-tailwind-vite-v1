2b48c9b4324c4afea7508a68f6c8285cef18a7fd
fajarfadillahh <fajaragustian979@gmail.com>
2022-09-29 11:42:57 +0700

    add dark mode

--- src/js/main.js
+++ src/js/main.js
@@ -24,3 +24,36 @@ function stickyHeader() {
     : header.classList.remove("sticky-action");
 }
 window.addEventListener("scroll", stickyHeader);
+
+// ===== DARK MODE =====
+var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+
+// change then icons inside the button based on previous settings
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  themeToggleLightIcon.classList.remove("hidden");
+} else {
+  themeToggleDarkIcon.classList.remove("hidden");
+}
+
+// theme toggle button
+var themeToggleBtn = document.getElementById("theme-toggle");
+
+themeToggleBtn.addEventListener("click", () => {
+  // toggle icons inside button
+  themeToggleLightIcon.classList.toggle("hidden");
+  themeToggleDarkIcon.classList.toggle("hidden");
+
+  // if setting via local storage previously
+  if (localStorage.theme === "light") {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  }
+});
